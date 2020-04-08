843d7c95bb118910ce26efaa4471c463466314a9
abc <abc@example.com>
2020-04-08 17:40:11 +0530

    Updates

---
+++ precache-manifest.73040d72e860e54109dab9a57e098d46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7757d3abc0164bd0f34c1dff97090988",
+    "url": "/Covid-19-India/index.html"
+  },
+  {
+    "revision": "e27647f27c4b8da1d5e1",
+    "url": "/Covid-19-India/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d96663ed9138f6b85557",
+    "url": "/Covid-19-India/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "e27647f27c4b8da1d5e1",
+    "url": "/Covid-19-India/static/js/2.5373a90a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Covid-19-India/static/js/2.5373a90a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d96663ed9138f6b85557",
+    "url": "/Covid-19-India/static/js/main.6017ef27.chunk.js"
+  },
+  {
+    "revision": "7ebc4c97f5a69c8cd3c9",
+    "url": "/Covid-19-India/static/js/runtime-main.38be7b48.js"
+  }
+]);

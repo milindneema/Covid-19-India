3b7750adea2efbde5e00cd0b819fb83f09e3402a
abc <abc@example.com>
2020-04-13 15:59:48 +0530

    On click on card of state it show district wise data in tabular format

--- src/components/StateComponent.js
+++ src/components/StateComponent.js
@@ -1,54 +1,73 @@
 import React, { Component } from 'react';
 import { Card, CardTitle, Badge, Progress, Row, Col } from 'reactstrap';
+import Table from './TableComponent'
 
 class States extends Component {
-    // async componentDidMount() {
 
-    //     const response = await fetch('https://api.covid19india.org/v2/state_district_wise.json');
-    //     const data = await response.json();
-    //     this.setState({ district: data })
-    // }
 
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            stateindex: null,
+            stateData: null,
+        }
+    }
+
+    handleState = (index) => {
+        if (this.state.stateindex === null) {
+            const state = this.props.total.statewise[index].state
+            console.log(state)
+            this.setState({ stateData: state, stateindex: index })
+        }
+        else {
+            this.setState({ stateData: null, stateindex: null })
+        }
+    }
 
     render() {
+        const card = this.props.total.statewise.map((state, index) => {
+            if (state.state !== "Total") {
+                const activePercent = (state.active / state.confirmed) * 100;
+                const recoveredPercent = (state.recovered / state.confirmed) * 100;
+                const deathsPercent = (state.deaths / state.confirmed) * 100;
+                return (
+                    <Col key={index} xs="12" sm="12" md="6" lg="4" >
+                        <Card body outline color="primary" className="shadow m-3 bg-white rounded" onClick={() => { this.handleState(index) }} >
+                            <CardTitle className="text-primary font-weight-bold">
+                                <h4>
+                                    {state.state} &nbsp;&nbsp;
+                                    <Badge pill color="primary">
+                                        &#8593; {state.deltaconfirmed}
+                                    </Badge>
+                                </h4>
+                            </CardTitle>
+                            <div>
+                                <div className="text-center font-weight-bold">confirmed : {state.confirmed}</div>
+                                <Progress style={{ height: "22px" }} value={state.confirmed !== "0" ? 100 : 0}>100%</Progress>
+                                <div className="text-center font-weight-bold">Recovered : {state.recovered}</div>
+                                <Progress style={{ height: "22px" }} color="success" value={recoveredPercent ? recoveredPercent : 0}>{recoveredPercent ? recoveredPercent.toFixed(1) : 0}%</Progress>
+                                <div className="text-center font-weight-bold">Active : {state.active}</div>
+                                <Progress style={{ height: "22px" }} color="warning" value={activePercent ? activePercent : 0}>{activePercent ? activePercent.toFixed(1) : 0}%</Progress>
+                                <div className="text-center font-weight-bold">Deaths : {state.deaths}</div>
+                                <Progress style={{ height: "22px" }} color="danger" value={deathsPercent ? deathsPercent : 0}>{deathsPercent ? deathsPercent.toFixed(1) : 0}%</Progress>
+                                <p className="text-muted">*This card is clickable</p>
+                            </div>
+                        </Card>
+                        {this.state.stateindex === index ? <Table stateDistrict={this.state.stateData} /> : <div></div>}
+                    </Col>
+                )
+            }
+            else {
+                return (
+                    <div key={state.state}></div>
+                )
+            }
+        })
         return (
-
             <Row>
-                {
-                    this.props.total.statewise.map((state) => {
-                        if (state.state !== "Total") {
-                            const activePercent = (state.active / state.confirmed) * 100;
-                            const recoveredPercent = (state.recovered / state.confirmed) * 100;
-                            const deathsPercent = (state.deaths / state.confirmed) * 100;
-                            return (
-                                <Col key={state.state} xs="12" sm="12" md="6" lg="4" >
-                                    <Card body outline color="primary" className="shadow m-3 bg-white rounded" >
-                                        <CardTitle className="text-primary font-weight-bold"><h4>{state.state} &nbsp;&nbsp;<Badge pill color="primary">&#8593; {state.deltaconfirmed}</Badge></h4></CardTitle>
-                                        <div>
-                                            <div className="text-center font-weight-bold">confirmed : {state.confirmed}</div>
-                                            <Progress style={{ height: "22px" }} value={state.confirmed !== "0" ? 100 : 0}>100%</Progress>
-                                            <div className="text-center font-weight-bold">Recovered : {state.recovered}</div>
-                                            <Progress style={{ height: "22px" }} color="success" value={recoveredPercent ? recoveredPercent : 0}>{recoveredPercent ? recoveredPercent.toFixed(1) : 0}%</Progress>
-                                            <div className="text-center font-weight-bold">Active : {state.active}</div>
-                                            <Progress style={{ height: "22px" }} color="warning" value={activePercent ? activePercent : 0}>{activePercent ? activePercent.toFixed(1) : 0}%</Progress>
-                                            <div className="text-center font-weight-bold">Deaths : {state.deaths}</div>
-                                            <Progress style={{ height: "22px" }} color="danger" value={deathsPercent ? deathsPercent : 0}>{deathsPercent ? deathsPercent.toFixed(1) : 0}%</Progress>
-                                        </div>
-                                    </Card>
-
-                                </Col>
-                            )
-                        }
-                        else {
-                            return (
-                                <div key={state.state}></div>
-                            )
-                        }
-                    })
-                }
+                {card}
             </Row >
-
-
         );
     }
 }
---
+++ src/components/TableComponent.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { Table, Spinner, Badge } from 'reactstrap';
+
+
+class TableContaint extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            loading: true,
+            district: null,
+        }
+    }
+
+    async componentDidMount() {
+
+        const response = await fetch('https://api.covid19india.org/v2/state_district_wise.json');
+        const data = await response.json();
+        if (this.props.stateDistrict === null) {
+            this.setState({ loading: false })
+        } else {
+            const state = data.filter(states => this.props.stateDistrict === states.state)[0];
+            this.setState({ district: state, loading: false })
+        }
+
+    }
+    render() {
+        return (
+            <div>
+                {this.state.loading ? <div className="d-flex justify-content-center">
+                    <Spinner color="primary" style={{ width: '5rem', height: '5rem' }} />
+                </div> : this.state.district !== null ?
+                        <Table size="sm" responsive borderless>
+                            <thead>
+                                <tr >
+                                    <th><h4><Badge color="dark">#</Badge></h4></th>
+                                    <th><h4><Badge color="dark">District</Badge></h4></th>
+                                    <th><h4><Badge color="dark">Confirmed</Badge></h4></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {this.state.district.districtData.map((districtData, index) => {
+                                    return (
+                                        <tr key={index}>
+                                            <td ><h5><Badge color="secondary" >{index + 1}</Badge></h5></td>
+                                            <td ><h5><Badge color="secondary">{districtData.district}</Badge></h5></td>
+                                            <td><h5><Badge color="secondary">{districtData.confirmed}</Badge></h5></td>
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>
+                        </Table> : <div></div>
+                }
+            </div >
+        )
+    }
+}
+
+export default TableContaint;
